017eb4d4f77943d4294a78f936b1d9e14843b9b9
riju <softwaretestnation@gmail.com>
2018-04-14 11:05:51 +0530

    commit from laptop

--- JavaDemo/src/com/horizoninfolab/CostCenterImplementation.java
+++ JavaDemo/src/com/horizoninfolab/CostCenterImplementation.java
@@ -6,7 +6,7 @@ public class CostCenterImplementation {
 	
 	CostCenter CC1 = new CostCenter();
 	CC1.setCcNumber(1);
-	CC1.setDescription("Cost Center For QA Operations");
+	CC1.setDescription("Cost Center For QA Operations is huge");
 	CC1.setName("QA Cost Center");
 	System.out.println(CC1.getName() +","+CC1.getDescription()+","+CC1.getDescription());
 	
